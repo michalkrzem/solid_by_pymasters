8e06bdd868bb6b21544ea13a65e3c4332a7dbe16
mkrzeminski <mkrzeminski@imgw.pl>
2024-04-15 10:39:27 +0200

    Searching mode added and tested

--- library_by_kvothe.py
+++ library_by_kvothe.py
@@ -1,6 +1,50 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass
 class Book:
-    def __init__(self, title, autor, isbn, theme):
-        self.title = title
-        self.autor = autor
-        self.isbn = isbn
-        self.theme = theme
+    id: int
+    title: str
+    author: str
+    isbn: int
+    theme: str
+
+
+class Searcher(ABC):
+    @abstractmethod
+    def search_book(self, author: str, catalog_by_kvothe):
+        pass
+
+
+class SearchByAuthor(Searcher):
+    def search_book(self, author: str, catalog_by_kvothe):
+        searching_books = []
+        for i in catalog_by_kvothe:
+            if i.__dict__["author"] == author:
+                searching_books.append(i)
+        return searching_books
+
+
+class SearchByTitle(Searcher):
+    def search_book(self, title: str, catalog_by_kvothe):
+        searching_books = []
+        for i in catalog_by_kvothe:
+            if i.__dict__["title"] == title:
+                searching_books.append(i)
+        return searching_books
+
+
+class LibraryCatalog:
+    def __init__(self, catalog_by_kvothe=None):
+        if catalog_by_kvothe is None:
+            catalog_by_kvothe = []
+        self.catalog_by_kvothe = catalog_by_kvothe
+        print(self.catalog_by_kvothe)
+
+    def add_book(self, book: Book):
+        self.catalog_by_kvothe.append(book)
+
+    def search_book_from_catalog(self, variable, searcher: Searcher):
+        return searcher.search_book(variable, self.catalog_by_kvothe)
+
--- test_library_by_kvothe.py
+++ test_library_by_kvothe.py
@@ -1,14 +1,85 @@
+import json
+
 import pytest
 
-from library_by_kvothe import Book
+from library_by_kvothe import Book, LibraryCatalog, SearchByAuthor, SearchByTitle
 
 
 def test_book_class():
     book = Book(
-        title="Name of the wind",
-        autor="Patrik Roofus",
+        id=1,
+        title="The name of the wind",
+        author="Patrick Rothfuss",
         isbn=12345,
         theme="Fantasy"
     )
     assert book
-    assert book.title == "Name of the wind"
+    assert book.title == "The name of the wind"
+
+
+def test_library_catalog():
+    library_catalog_manager = LibraryCatalog()
+
+    assert library_catalog_manager
+
+
+def test_add_book_to_catalog():
+    book_2 = Book(
+        id=3,
+        title="The name of the wind",
+        author="Patrick Rothfuss",
+        isbn=12345,
+        theme="Fantasy"
+    )
+    library_catalog_manager = LibraryCatalog()
+    library_catalog_manager.add_book(book_2)
+
+    assert library_catalog_manager
+    assert library_catalog_manager.catalog_by_kvothe[0].title == "The name of the wind"
+
+
+def test_search_book():
+    book_3 = Book(
+        id=2,
+        title="The name of the wind",
+        author="Patrick Rothfuss",
+        isbn=12345,
+        theme="Fantasy"
+    )
+    library_catalog_manager = LibraryCatalog()
+    library_catalog_manager.add_book(book_3)
+
+    searcher = SearchByAuthor()
+    searcher.search_book("Patrick", library_catalog_manager.catalog_by_kvothe)
+
+
+def test_search_book_by_author():
+    book_4 = Book(
+        id=4,
+        title="The name of the wind",
+        author="Patrick Rothfuss",
+        isbn=125445,
+        theme="Fantasy"
+    )
+    library_catalog_manager = LibraryCatalog()
+    library_catalog_manager.add_book(book_4)
+
+    author_searcher = SearchByAuthor()
+    searching_book = library_catalog_manager.search_book_from_catalog("Patrick Rothfuss", author_searcher)
+    assert book_4 in searching_book
+
+
+def test_search_book_by_title():
+    book_5 = Book(
+        id=4,
+        title="The name of the wind",
+        author="Patrick Rothfuss",
+        isbn=125445,
+        theme="Fantasy"
+    )
+    library_catalog_manager = LibraryCatalog()
+    library_catalog_manager.add_book(book_5)
+
+    title_searcher = SearchByTitle()
+    searching_book = library_catalog_manager.search_book_from_catalog("The name of the wind", title_searcher)
+    assert book_5 in searching_book
